0dbad3f10e14faf4b146efe19ba42c54f50e0608
dopey2 <daniel.gabor@supertich.fr>
2020-11-02 21:44:24 +0100

    first commit

---
+++ desktop/src/com/flappybird/game/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.flappybird.game.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.flappybird.game.FlappyBird
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(FlappyBird(), config)
+    }
+}
